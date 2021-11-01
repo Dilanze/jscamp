611e8997c1667bbb28bc4d0e5a43cf36364535b4
Dilanze <92622506+Dilanze@users.noreply.github.com>
2021-11-01 22:10:32 +0300

    javascript
    javascript

---
+++ src/app.js
@@ -0,0 +1,70 @@
+console.log("Merhaba Kodlamaa")
+
+// let dolarDun = 9.20
+// let dolarBug = 9.30
+// {
+//     let dolarDun = 9.10
+// }
+
+// console.log(dolarDun)
+// const euroDun = 11.2
+// euroDun = 11// yapılmaz
+// console.log(euroDun)
+
+// var her seyı kabul eder scope içindeki dahil ve kullanılması ççok önerilmez let scope içindekini çalıştırmaz ve kullanılması tavsıye edılır ; kullanılmıyo kodlarda
+// js type safe değildir yani tip güvenliği tip tanımlaması çok önemli değil bence tip güvenliği olmalı ve kullanılmalı
+
+// Array, tek tırnakta kabul edilir
+// camelCasing kullanılır Javada
+// PascalCasing C# method isimleri
+// let konutKredileri = ["Konut kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+// console.log()
+
+// for (let i = 0; i < 3; i++) {
+//     console.log("")
+
+// }
+
+//let users = getUsersFromApi() // array vericek
+// console.log(konutKredileri)
+// json placeholder.com users girdi
+
+// auto tag bracket 2 dracula editor config vscode bu eğitimde kullanılmayacak es7  react material ıcon theme
+
+// tip güvenli bir dil değil, typescript tip güvenli bir dil, oynatma listesi jscript dersleri
+// state managemnet sepetle kelnın aynı anda guncellenmesi
+
+// var her sey kabul, let sınırlı kullanım, const, ; dert değil
+
+// control kc , control ku
+
+let sayi1 = 10
+sayi1 = "Engin"
+let student = { id: 1, name: "Engin" }
+//console.log(student)
+
+function save(ogrenci, puan = 10) {
+
+    console.log(ogrenci.name + "" + puan) //undefıned hata değil
+
+}
+save(student, 100); // çıktı engin : 100 olur
+
+let showProducts = function (id,) {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/functıons.js
@@ -0,0 +1,22 @@
+
+function addToCart(productName = "Elma", quantity) {
+    console.log("Sepete EKlendi" + productName + "adet: " + quantity)
+}
+// addToCart("Elma")
+addToCart("Yumurta", 10)
+// addToCart("Karpuz")
+
+let sayHello = () => {
+    console.log("Hello world")
+}
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello world 2 ")
+}
+sayHello2();
+
+
+
+
+
